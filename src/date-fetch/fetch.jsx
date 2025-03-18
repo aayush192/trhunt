e4d7dfeb5fbe6c61cc18371c2c0e42a2b44f0174
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-18 19:47:36 +0545

    Update fetch.jsx

--- src/date-fetch/fetch.jsx
+++ src/date-fetch/fetch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const getData=async(data)=>{
-    const response= await fetch(`http://trhuntapi-production.up.railway.app/game/${data}`)
+    const response= await fetch(`https://trhuntapi-production.up.railway.app/game/${data}`)
     console.log(response.json());
 }
 export default getData;

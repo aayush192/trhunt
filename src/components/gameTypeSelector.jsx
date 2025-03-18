05f108cd26c7babc831f12a4614fcf0df995528f
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-18 18:00:05 +0545

    Update gameTypeSelector.jsx

--- src/components/gameTypeSelector.jsx
+++ src/components/gameTypeSelector.jsx
@@ -1,18 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-const GameTypeSelector = () => {
-  return (
-    <div className="game-type-selector">
-      {['A', 'B', 'C'].map((type) => (
-        <div className="type-card" key={type}>
-          <div className="icon">✏️</div>
-          <h3>Type {type}</h3>
-          <p>The description of the game to be played in the type item and questions asked and prize</p>
-        </div>
-      ))}
-      <button className="proceed-btn">Proceed</button>
-=======
 import { useState } from 'react';
 import getData from '../date-fetch/fetch';
 const GameTypeSelector = () => {
@@ -44,7 +30,6 @@ const GameTypeSelector = () => {
         getData(data);
         console.log(data);
       }}>Proceed</button>
->>>>>>> commit changes
     </div>
   );
 };

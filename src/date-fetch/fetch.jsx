f3e66863c2007804ab27b9b455547a3a6e79fda5
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-18 18:00:49 +0545

    Update fetch.jsx

--- src/date-fetch/fetch.jsx
+++ src/date-fetch/fetch.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import { useState } from "react";
 
-const getData=async()=>{
-    const response= await fetch('https://trhuntapi-production.up.railway.app')
-    console.log(response.json());
-}
-export default getData;
-=======
 
 const getData=async(data)=>{
     const response= await fetch(`http://trhuntapi-production.up.railway.app/game/${data}`)
     console.log(response.json());
 }
 export default getData;
->>>>>>> commit changes
+

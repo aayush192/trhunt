c0bd4863628c8d9d2aac578b999096958d7e223d
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-21 15:19:52 +0545

    if game is completed then what to do

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,34 @@ import React from 'react';
 import { NavLink,Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <div className="header flex justify-evenly">
-    
-      <NavLink to={''} className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ?"text-orange-700 border-b-3 border-blue-400":"text-gray-600" } font-bold  hover:text-orange-800  lg:p-0`
-                                    }>Game Type</NavLink>
-      <NavLink to={'clueDisplay'} className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ?"text-orange-700 border-b-3 border-blue-400":"text-gray-600" } font-bold  hover:text-orange-800  lg:p-0`
-                                    }>Show Clue</NavLink>
-     </div>
+    <div className="header flex justify-center gap-8 bg-gray-100 py-4 shadow-md">
+  <NavLink
+    to={''}
+    className={({ isActive }) =>
+      `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
+        isActive
+          ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
+          : "text-gray-600 hover:text-orange-800"
+      }`
+    }
+  >
+    Game Type
+  </NavLink>
+  
+  <NavLink
+    to={'clueDisplay'}
+    className={({ isActive }) =>
+      `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
+        isActive
+          ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
+          : "text-gray-600 hover:text-orange-800"
+      }`
+    }
+  >
+    Show Clue
+  </NavLink>
+</div>
+
   );
 };
 

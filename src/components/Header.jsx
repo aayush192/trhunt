26529431a2518e2afb501911a3dd6d75a14db5ac
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-21 17:20:23 +0545

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <div className="header flex justify-center gap-8 bg-gray-100 py-4 shadow-md">
-  <NavLink
-    to={''}
-    className={({ isActive }) =>
-      `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
-        isActive
-          ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
-          : "text-gray-600 hover:text-orange-800"
-      }`
-    }
-  >
-    Game Type
-  </NavLink>
-  
-  <NavLink
-    to={'clueDisplay'}
-    className={({ isActive }) =>
-      `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
-        isActive
-          ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
-          : "text-gray-600 hover:text-orange-800"
-      }`
-    }
-  >
-    Show Clue
-  </NavLink>
-</div>
+    <div className="header flex justify-between items-center px-8 py-4 bg-gray-100 shadow-md">
+      <div className="flex items-center gap-2">
+        <span className="text-3xl font-extrabold text-orange-700 tracking-wide">
+          <NavLink to="/">
+          Trhunt
+          </NavLink>
+        </span>
+      </div>
 
+      {/* Navigation Links */}
+      <div className="flex justify-center gap-8">
+        <NavLink
+          to={''}
+          className={({ isActive }) =>
+            `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
+              isActive
+                ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
+                : "text-gray-600 hover:text-orange-800"
+            }`
+          }
+        >
+          Game Type
+        </NavLink>
+        
+        <NavLink
+          to={'clueDisplay'}
+          className={({ isActive }) =>
+            `relative block py-2 px-4 text-lg font-bold duration-200 rounded-md transition-all ${
+              isActive
+                ? "text-orange-700 after:absolute after:w-full after:h-[3px] after:bg-blue-400 after:bottom-0 after:left-0"
+                : "text-gray-600 hover:text-orange-800"
+            }`
+          }
+        >
+          Show Clue
+        </NavLink>
+      </div>
+    </div>
   );
 };
 

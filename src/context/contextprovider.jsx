aa62e14e755c4a029fe4662546509fc0791ed8e2
aayush192 <140570881+aayush192@users.noreply.github.com>
2025-03-26 23:55:48 +0545

    Update contextprovider.jsx

--- src/context/contextprovider.jsx
+++ src/context/contextprovider.jsx
@@ -5,8 +5,9 @@ const Provider=({children})=>{
     const [data,setData]=useState(null);
     const[val,setVal]=useState(null)
      const [response, setResponse] = useState(null);
+   const [ansresponse, setAnsResponse] = useState(null);
     return(
- <userContext.Provider value={{data,setData,val,setVal,response,setResponse}}>
+ <userContext.Provider value={{data,setData,val,setVal,response,setResponse,ansresponse,setAnsResponse}}>
     {children}
  </userContext.Provider>
     )
